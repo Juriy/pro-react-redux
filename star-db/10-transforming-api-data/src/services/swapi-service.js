2208d771eafd9b4b99d0ab8ea697bac24a46aba1
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2021-04-28 19:59:18 +0300

    New address for api

--- star-db/10-transforming-api-data/src/services/swapi-service.js
+++ star-db/10-transforming-api-data/src/services/swapi-service.js
@@ -1,6 +1,6 @@
 export default class SwapiService {
 
-  _apiBase = 'https://swapi.co/api';
+  _apiBase = 'https://swapi.dev/api';
 
   async getResource(url) {
     const res = await fetch(`${this._apiBase}${url}`);
--- star-db/140-dynamic-context-switch/src/services/swapi-service.js
+++ star-db/140-dynamic-context-switch/src/services/swapi-service.js
@@ -1,6 +1,6 @@
 export default class SwapiService {
 
-  _apiBase = 'https://swapi.co/api';
+  _apiBase = 'https://swapi.dev/api';
   _imageBase = 'https://starwars-visualguide.com/assets/img';
 
   getResource = async (url) => {
