ec6224c69776ee0715dd2e57ddf013564e6fb029
madekot <mgolubcov@gmail.com>
2021-10-09 16:55:55 +0300

    state состояние реакт компонента

--- build-sandbox/10-empty-project/todo/src/components/todo-list-item/todo-list-item.js
+++ build-sandbox/10-empty-project/todo/src/components/todo-list-item/todo-list-item.js
@@ -3,34 +3,41 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false,
+  };
+
   onLabelClick = () => {
-    console.log(this.props.label)
-  }
+    this.setState((state) => {
+      return {done: !state.done};
+    });
+  };
 
   render() {
-    const { label, important = false } = this.props;
-    
+    const {label, important = false} = this.props;
+    const {done} = this.state;
+
     const style = {
       color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
+      fontWeight: important ? 'bold' : 'normal',
     };
 
+    const classes = ['todo-list-item'];
+    if (done) {
+      classes.push('done');
+    }
+
     return (
-      <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}>
+      <span className={classes.join(' ')}>
+        <span className="todo-list-item-label" style={style} onClick={this.onLabelClick}>
           {label}
         </span>
-  
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+
+        <button type="button" className="btn btn-outline-success btn-sm float-right">
           <i className="fa fa-exclamation" />
         </button>
-  
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+
+        <button type="button" className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o" />
         </button>
       </span>
