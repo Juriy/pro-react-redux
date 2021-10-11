53c9444ee1579451b71199b0142d9ec8f7237e5b
madekot <mgolubcov@gmail.com>
2021-10-11 16:00:54 +0300

    Контролируемые компоненты

--- build-sandbox/10-empty-project/todo/src/components/item-add-form/item-add-form.js
+++ build-sandbox/10-empty-project/todo/src/components/item-add-form/item-add-form.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+  state = {
+    itemText: '',
+  };
   
-  render() {
-    const {onAddItemClick} = this.props;
+  onItemFildInput = (evt) => {
+    this.setState({itemText: evt.target.value})
+  }
+  
+  onFormSubmit = (evt) => {
+    evt.preventDefault();
+    this.props.onAddItemClick(this.state.itemText);
+    this.setState({itemText: ''});
+  }
 
+  render() {
     return (
-      <div className="item-add-form">
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => onAddItemClick('message: hello world!')}>
+      <form
+        className="item-add-form"
+        onSubmit={this.onFormSubmit}
+      >
+        <input
+          className="form-control"
+          type="text"
+          value={this.state.itemText}
+          placeholder="enter the text of the task item"
+          onInput={this.onItemFildInput}
+        />
+        <button className="btn btn-outline-secondary" type="submit">
           Add Item
         </button>
-      </div>
-    )
+      </form>
+    );
   }
 }
--- build-sandbox/10-empty-project/todo/src/components/search-panel/search-panel.js
+++ build-sandbox/10-empty-project/todo/src/components/search-panel/search-panel.js
@@ -3,11 +3,7 @@ import React from 'react';
 import './search-panel.css';
 
 const SearchPanel = () => {
-  return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
-  );
+  return <input type="text" className="form-control search-input" placeholder="type to search" />;
 };
 
 export default SearchPanel;
