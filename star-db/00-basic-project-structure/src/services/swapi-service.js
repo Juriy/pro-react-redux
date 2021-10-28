7ae4fe75ca01fa58c0f48af257ecf4e8c0d5f32c
madekot <mgolubcov@gmail.com>
2021-10-28 22:00:06 +0300

    w&p 58: Трансформация данных API

--- star-db/00-basic-project-structure/src/services/swapi-service.js
+++ star-db/00-basic-project-structure/src/services/swapi-service.js
@@ -23,11 +23,14 @@ export default class SwapiService {
 
   async getAllPlanets() {
     const res = await this.getResource(`/planets/`);
-    return res.results;
+    return res.results.map(this._transformPlanet);
   }
 
   getPlanet(id) {
     return this.getResource(`/planets/${id}/`);
+
+    // const planet = await this.getResource(`/planets/${id}/`);
+    // return this._transformPlanet(planet);
   }
 
   async getAllStarships() {
@@ -38,4 +41,16 @@ export default class SwapiService {
   getStarship(id) {
     return this.getResource(`/starships/${id}/`);
   }
+
+  // _transformPlanet() {
+  //   const idRegExp = /\/([0-9]*)\/$/;
+  //   const id = planet.url.match(idRegExp)[1];
+  //   return {
+  //     id: 2,
+  //     name: planet.name,
+  //     population: planet.population,
+  //     rotation: planet.rotation_period,
+  //     diameter: planet.diameter,
+  //   }
+  // }
 }
