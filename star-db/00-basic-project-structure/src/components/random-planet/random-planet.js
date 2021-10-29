58d9354c6d9ed90018b18e6303dcf35c11b87d9d
Михаил Голубцов <mgolubcov@gmail.com>
2021-10-29 18:10:07 +0300

    fin: 61. Обработка ошибок сети в компоненте

--- star-db/00-basic-project-structure/src/components/random-planet/random-planet.js
+++ star-db/00-basic-project-structure/src/components/random-planet/random-planet.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
 import './random-planet.css';
+
 import SwapiService from '../../services/swapi-service'
 import Spinner from '../spinner/spinner';
+import ErrorIndicator from '../error-indicator/error-indicator';
+
 
 export default class RandomPlanet extends Component {
 
@@ -11,6 +14,7 @@ export default class RandomPlanet extends Component {
   state = {
     planet: {},
     loading: true,
+    error: false,
   }
 
   constructor() {
@@ -22,25 +26,39 @@ export default class RandomPlanet extends Component {
     this.setState({planet, loading: false})
   };
 
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    });
+  }
+
   updatePlanet() {   
-    const id = Math.floor(Math.random()*25) + 2;
+    // const id = Math.floor(Math.random()*25) + 2;
+    const id = 122213;
     this.swapiService
       .getPlanet(id)  
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   };
   
   render() {
     const {
       planet,
       loading,
+      error,
     } = this.state;
 
+    const hasData = !(loading || error)
+
+    const errorMessage = error ? <ErrorIndicator />: null;
     const spinner = loading ? <Spinner />: null;
-    const content = !loading ? <PlanetView planet={planet} />: null;
+    const content = hasData ? <PlanetView planet={planet} />: null;
 
     return (     
       <div className="random-planet jumbotron rounded">
         {spinner}
+        {errorMessage}
         {content}
       </div>
     );
