58a0bc5e88c296109216bd94bb49b30a475e9833
madekot <mgolubcov@gmail.com>
2021-10-30 18:46:52 +0300

    fin: 66. Используем на практике componentDidMount()

--- star-db/00-basic-project-structure/src/components/app/app.js
+++ star-db/00-basic-project-structure/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
@@ -7,26 +7,42 @@ import PersonDetails from '../person-details';
 
 import './app.css';
 
-const App = () => {
-  const onPersonSelected = (id) => {
-    console.log(id)
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: null,
   }
 
-  return (
-    <div>
-      <Header />
-      <RandomPlanet />
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };  
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={onPersonSelected}/>
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <RandomPlanet />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  };
 };
-
-export default App;
