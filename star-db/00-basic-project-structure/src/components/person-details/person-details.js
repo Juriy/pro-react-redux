fc23b65a709dfab2348d1a5a5b581e1da3244e12
madekot <mgolubcov@gmail.com>
2021-10-30 22:05:58 +0300

    w&p 68. Используем на практике componentDidUpdate(): 4:56

--- star-db/00-basic-project-structure/src/components/person-details/person-details.js
+++ star-db/00-basic-project-structure/src/components/person-details/person-details.js
@@ -20,9 +20,7 @@ export default class PersonDetails extends Component {
       .getPerson(personId)
       .then((person) => {
         this.setState({person});
-      })
-
-        
+      })     
   }
 
   componentDidMount() {
@@ -30,25 +28,31 @@ export default class PersonDetails extends Component {
   }
   
   render() {
+    const {id, name, gender, birthYear, eyeColor} = this.state.person || {};
+
+    if (!this.state.person) {
+      return <span>Select a person from a list</span>;
+    }
+
     return (
       <div className="person-details card">
         <img className="person-image" alt=""
-          src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
 
         <div className="card-body">
-          <h4>R2-D2</h4>
+          <h4>{name} {this.props.personId}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Gender</span>
-              <span>male</span>
+              <span>{gender}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Birth Year</span>
-              <span>43</span>
+              <span>{birthYear}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Eye Color</span>
-              <span>red</span>
+              <span>{eyeColor}</span>
             </li>
           </ul>
         </div>
--- star-db/00-basic-project-structure/src/services/swapi-service.js
+++ star-db/00-basic-project-structure/src/services/swapi-service.js
@@ -75,8 +75,8 @@ export default class SwapiService {
       id: this._extractId(person),
       name: person.name,
       gender: person.gender,
-      birthYear: person.birthYear,
-      eyeColor: person.eyeColor
+      birthYear: person.birth_year,
+      eyeColor: person.eye_color
     }
   }  
 }
