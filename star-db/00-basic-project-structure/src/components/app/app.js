d721d112210a38fdc6a6893dd5575d3b8279e09f
madekot <mgolubcov@gmail.com>
2021-10-31 09:30:34 +0300

    68. fin: Используем на практике componentDidUpdate()

--- star-db/00-basic-project-structure/src/components/app/app.js
+++ star-db/00-basic-project-structure/src/components/app/app.js
@@ -11,7 +11,7 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: 1,
+    selectedPerson: null,
   }
 
   toggleRandomPlanet = () => {
--- star-db/00-basic-project-structure/src/components/person-details/person-details.js
+++ star-db/00-basic-project-structure/src/components/person-details/person-details.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 import './person-details.css';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner/spinner';
 
 export default class PersonDetails extends Component {
   swapiService = new SwapiService();
 
   state = {
-    person: null
+    person: null,
+    hasLoading: false
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.personId !== prevProps.personId) {
+      this.updatePerson();
+    }
   }
 
   updatePerson() {
@@ -16,10 +24,12 @@ export default class PersonDetails extends Component {
       return;
     }
     
+    this.setState({hasLoading: true})
+
     this.swapiService
       .getPerson(personId)
       .then((person) => {
-        this.setState({person});
+        this.setState({person, hasLoading: false});
       })     
   }
 
@@ -28,35 +38,51 @@ export default class PersonDetails extends Component {
   }
   
   render() {
-    const {id, name, gender, birthYear, eyeColor} = this.state.person || {};
-
-    if (!this.state.person) {
-      return <span>Select a person from a list</span>;
-    }
-
+    const {person, hasLoading} = this.state
+    const hasData = person && !hasLoading;
+    
+    const lolderIndicator = hasLoading ? <Spinner />: null;
+    const content = hasData ? <PersonDetailsContent person={person} /> : null;
+    const emptyContent = person ? null: <EmptyContent />;
+    
     return (
       <div className="person-details card">
-        <img className="person-image" alt=""
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
-
-        <div className="card-body">
-          <h4>{name} {this.props.personId}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>{eyeColor}</span>
-            </li>
-          </ul>
-        </div>
+        {lolderIndicator}
+        {content}
+        {emptyContent}
       </div>
     )
   }
+}
+
+const PersonDetailsContent = ({person}) => {
+  const {id, name, gender, birthYear, eyeColor} = person;
+  return (
+    <React.Fragment>
+      <img className="person-image" alt=""
+      src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+
+      <div className="card-body">
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Gender</span>
+            <span>{gender}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Birth Year</span>
+            <span>{birthYear}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Eye Color</span>
+            <span>{eyeColor}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
+}
+
+const EmptyContent = () => {
+  return <span>Select a person from a list</span>;
 }
