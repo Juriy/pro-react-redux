e9c50e899b1970585ea7401f8c2a38b4949892c0
madekot <mgolubcov@gmail.com>
2021-10-31 11:06:41 +0300

    fin: 70. componentDidCatch()

--- star-db/00-basic-project-structure/src/components/app/app.js
+++ star-db/00-basic-project-structure/src/components/app/app.js
@@ -4,14 +4,16 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
+import ErrorButton from '../error-button/error-button';
+import ErrorIndicator from '../error-indicator/error-indicator';
 
 import './app.css';
 
 export default class App extends Component {
-
   state = {
     showRandomPlanet: true,
     selectedPerson: 5,
+    hasRenderError: false,
   }
 
   toggleRandomPlanet = () => {
@@ -28,11 +30,21 @@ export default class App extends Component {
     })
   }
 
+  componentDidCatch() {
+    this.setState({hasRenderError: true})
+  }
+  
   render() {
+
+    if (this.state.hasRenderError) {
+      return <ErrorIndicator />
+    }
+    
     return (
       <div>
         <Header />
         <RandomPlanet />
+        <ErrorButton />
 
         <div className="row mb2">
           <div className="col-md-6">
