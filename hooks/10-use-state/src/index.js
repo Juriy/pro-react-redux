68d184abb09d24e5dfd905ecca7f481fd65e1aee
Михаил Голубцов <mgolubcov@gmail.com>
2021-11-08 15:25:30 +0300

    97. useState()

--- hooks/10-use-state/src/index.js
+++ hooks/10-use-state/src/index.js
@@ -1,7 +1,37 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <p>Hi</p>
+const App = () => {
+  return (
+    <div>
+      <HookSwither />
+    </div>
+  );
+};
 
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+const HookSwither = () => {
+  const [color, setColor] = useState('gray');
+  const [fontSize, setFontSize] = useState(14);
+
+  return (
+    <div
+      style={{
+        padding: '10px',
+        backgroundColor: color,
+        fontSize: `${fontSize}px`, }}>
+      Hello React
+      <button onClick={() => setColor('gray')}>
+        Dark
+      </button>
+      <button onClick={() => setColor('white')}>
+        Light
+      </button>
+      <button
+        onClick={() => setFontSize((s) => s + 2)}>
+      +
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
